87662028bfb6621bd3cb85b61c852edcaaab788f
mprybylski <mprybylski@ivminc.com>
2022-05-18 23:46:36 -0400

    started on database editor

--- SQLITE_Editor2000/SQLITE_Editor2000/obj/Debug/net6.0-windows/View/CreateDataBaseWindow.g.i.cs
+++ SQLITE_Editor2000/SQLITE_Editor2000/obj/Debug/net6.0-windows/View/CreateDataBaseWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace SQLITE_Editor2000.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/SQLITE_Editor2000;component/view/createdatabasewindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SQLITE_Editor2000;V1.0.0.0;component/view/createdatabasewindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\CreateDataBaseWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
